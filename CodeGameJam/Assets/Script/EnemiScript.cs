e5b9029a8b8b7e48543665ca935680ab5e3c8be3
Lupusanghrenn <brisingr34130@gmail.com>
2022-01-27 00:31:05 +0100

    gitingore and enemy improvement

--- CodeGameJam/Assets/Script/EnemiScript.cs
+++ CodeGameJam/Assets/Script/EnemiScript.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class EnemiScript : MonoBehaviour
 {
+
+    private float test = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Log at start");
     }
 
     // Update is called once per frame
