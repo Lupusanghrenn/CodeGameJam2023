8667acae3a9bf7d8cff23b08116b02b623450d71
Lupusanghrenn <brisingr34130@gmail.com>
2022-02-02 00:36:17 +0100

    prefab and adding text mesh pro

--- CodeGameJam/Assets/Script/EnemiScript.cs
+++ CodeGameJam/Assets/Script/EnemiScript.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EnemiScript : MonoBehaviour
 {
+    public TextMeshProUGUI text;
     private float test = 0f;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Log at start");
+        text.text = "Hello World";
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Log in update");
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //Fire a projectile
+        }
+
+        float X = Input.GetAxis("Horizontal");
+        transform.position += Vector3.right * X * Time.deltaTime;
     }
 }
